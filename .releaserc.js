a6725a26af25067439dd25248d2568d7c922cc65
杨成锴 <homeboyc@foxmail.com>
2023-03-27 22:54:01 +0800

    update: recover all the changes about release workflow

--- .releaserc.js
+++ .releaserc.js
@@ -1,1 +1,8 @@
-module.exports = {};
+module.exports = {
+    branches: ["main", {name: 'alpha', prerelease: true}, {name: 'beta', prerelease: true}],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+};
